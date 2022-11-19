f2e6c35cfdd212758990c9fdb63a52f112f06e6b
Erivan Buenaventura <109859566+evan-07@users.noreply.github.com>
2022-11-20 00:24:32 +0800

    Added modules in mynetwork.tf

--- Terraform/mynetwork.tf
+++ Terraform/mynetwork.tf
@@ -18,3 +18,17 @@ allow {
     }
 source_ranges = ["0.0.0.0/0"]
 }
+# Create the mynet-us-vm instance
+module "mynet-us-vm" {
+  source           = "./instance"
+  instance_name    = "mynet-us-vm"
+  instance_zone    = "us-west1-c"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+# Create the mynet-eu-vm" instance
+module "mynet-eu-vm" {
+  source           = "./instance"
+  instance_name    = "mynet-eu-vm"
+  instance_zone    = "europe-west1-d"
+  instance_network = google_compute_network.mynetwork.self_link
+}

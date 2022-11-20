8efe56db2233de932f93b1a572fbea1ff54c7bdb
Erivan Buenaventura <109859566+evan-07@users.noreply.github.com>
2022-11-20 21:07:06 +0800

    updated main.tf

--- Terraform/03_IaC_with_Terraform/main.tf
+++ Terraform/03_IaC_with_Terraform/main.tf
@@ -29,8 +29,9 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = google_compute_network.vpc_network.self_link
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 }
@@ -40,3 +41,31 @@ resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
 }
 
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "{var.project}_bucket"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
